19be8595e2347bea4ca14befc2525487e6e77f6d
Julien-G-Man <juliengmanana@gmail.com>
2025-09-04 03:10:29 +0100

    Add logo

--- core/views.py
+++ core/views.py
@@ -1,17 +1,32 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
+from django.views import generic
 from django.contrib import messages
 from .models import NewsletterSubscription
 from .models import Scholarships
 
-# Create your views here.
+# Create your views here.   
 def home(request):
    featured_scholarships = Scholarships.objects.order_by('-created_at')[:3]
    context = {
       'featured_scholarships': featured_scholarships
    }
    return render(request, 'core/home.html', context)
+
+# Using Generic
+"""
+class home(generic.ListView):
+   template_name = 'core/home.html'
+   
+   def get_queryset(self):
+      return Scholarships.objects.all()
+      
+"""
+# here we use class instead of function
+# remove comments and replace the home function
+# you'll need to exchange the "featured_scholarships" in home.html, 
+# and "all_scholarships" inn scholarships.html with object_list
 
 def scholarships(request):
    all_scholarships = Scholarships.objects.all()
